30a69a57d7c0b44ab9e0b463d54f907185ff9d0a
weizenan <weizenan@mibook.com>
2018-11-18 12:47:42 +0800

    create project

---
+++ src/main/java/com/willpan/citystarsq/CitystarsqApplication.java
@@ -0,0 +1,12 @@
+package com.willpan.citystarsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitystarsqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitystarsqApplication.class, args);
+    }
+}
